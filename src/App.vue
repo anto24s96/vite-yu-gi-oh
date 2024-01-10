42a41a1483973178f336824dca28ccc0feb43b30
anto24s96 <anto24s96@gmail.com>
2024-01-10 16:36:02 +0100

    added Header section

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    components: {
+        AppHeader
+    }
+}
+</script>
+<template lang="">
+    <div>
+        <AppHeader></AppHeader>
+    </div>
+</template>
+<style lang="scss">
+@use "./styles/generals.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+<template lang="">
+    <header>
+        <div class="container-logo-title d-flex">
+            <img src="https://4.bp.blogspot.com/-aQgkqbEBQT8/Voq3RbKokzI/AAAAAAAACdo/pS7sxosX5BA/s1600/Logo%2BYu-Gi-Oh%2521%2B03.png" alt="yugioh-logo">
+            <h1>Yu-Gi-Oh Api</h1>
+        </div>
+    </header>
+</template>
+<style lang="scss" scoped>
+header {
+    background-color: white;
+    padding: 30px 20px;
+
+    .container-logo-title {
+        align-items: center;
+
+        img {
+            width: 100px;
+            margin-right: 20px;
+        }
+
+        h1 {
+            font-size: 50px;
+        }
+    }
+}
+</style>
